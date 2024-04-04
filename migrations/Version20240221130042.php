dafcbc125ffc1050bee84dfac63b18943b860dd7
JoseWongg <j.l.wong@edu.salford.ac.uk>
2024-04-04 18:44:53 +0100

    pagination and filters in Quiz repository and My Quizzes pages

--- migrations/Version20240221130042.php
+++ migrations/Version20240221130042.php
@@ -20,14 +20,14 @@ final class Version20240221130042 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE question ADD case_scenario LONGTEXT NOT NULL');
-        $this->addSql('ALTER TABLE quiz DROP title, DROP case_scenario, DROP is_approved');
+        //$this->addSql('ALTER TABLE question ADD case_scenario LONGTEXT NOT NULL');
+        //$this->addSql('ALTER TABLE quiz DROP title, DROP case_scenario, DROP is_approved');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE question DROP case_scenario');
-        $this->addSql('ALTER TABLE quiz ADD title VARCHAR(255) NOT NULL, ADD case_scenario LONGTEXT NOT NULL, ADD is_approved TINYINT(1) NOT NULL');
+        //$this->addSql('ALTER TABLE question DROP case_scenario');
+        //$this->addSql('ALTER TABLE quiz ADD title VARCHAR(255) NOT NULL, ADD case_scenario LONGTEXT NOT NULL, ADD is_approved TINYINT(1) NOT NULL');
     }
 }
